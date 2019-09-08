93fd14840bee96f658561ef8dad11d40ed8a22db
Peter Sulzen <psulzen@gmail.com>
2019-09-08 16:02:10 -0500

    final commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,7 +9,7 @@ import csv
 mainpath = os.path.join('Resources', 'budget_Data.csv')
 
 # output file define and stored
-output_path = os.path.join('Resources', "new.txt")
+output_path = os.path.join('Resources', "Budget_Results.txt")
 
 # Open the CSV file
 with open(mainpath) as csvfile:
@@ -20,12 +20,10 @@ with open(mainpath) as csvfile:
     # Save off the header row, just in case we want it.  Which we won't this time.   
     csv_header = next(csvreader)
 
-    # Read each row of data after the header
+    # Initilize all of the variables
     rowcount = 0     # so I know how many months there are
     run_tot = 0      # so I can get a running total of Profits and losses 
-    prev_val = 0     # I need to keep track of the previous value to calculate the change     
-   # firstrowf = True # I need to exclud the first row when I calc the change since there is no prior row
-    
+    prev_val = 0     # I need to keep track of the previous value to calculate the change      
     change = 0               # Initilize the change to a zero
     change_tot = 0         # needed to calculate average of total change 
     greatest_increase_amt = 0 # needed to calculate average of total change 
@@ -33,38 +31,44 @@ with open(mainpath) as csvfile:
     greatest_decrease_amt = 0 # needed to calculate average of total change
     greatest_decrease_mon = 0 # needed to calculate average of total change 
     
+    # loop through the data
     for row in csvreader:
+
+            # keep a running total of the row count
             rowcount += 1 
-            #run_tot = run_tot + int(row[1])
+
+            # keep a running total of the Profits and losses 
             run_tot += int(row[1])
+
+            # This will do calculations based on the change. The first row will not have 
+            # a change yet, so only do if this is not the first row 
             if prev_val != 0:
+
+                # Get the change by subtracting the previous value from the current
                 change = int(row[1]) - prev_val
-                change_tot = change_tot + change
+
+                # Keep a running total of the change 
+                change_tot += change
+
+                # Adjust the greatest increase amount as we go by saving it to a variable.
                 if change > greatest_increase_amt:
                     greatest_increase_amt = change
                     greatest_increase_mon = row[0]
+
+                # Adjust the greatest decrease amount as we go by saving it to a variable.
                 if change < greatest_decrease_amt:
                     greatest_decrease_amt = change
                     greatest_decrease_mon = row[0]
 
-
+            # Finally, we save the current value to "prev_val" so next loop we can determine the change
             prev_val = int(row[1])
-            #firstrowf = False
 
-
-            #x == x + 1 ' == would check if they are equal 
-            #print("total number of months is " row[0])
-    #print (row[0])
-    #print (row[1])
-    #print(len(row))
+    # We are ready to print 
     print(f"Financial Analysis")
     print(f"------------------")
     print(f"Total months: {rowcount}")
     print(f"Total: {run_tot}")
-#    print(f"Average Change: {run_tot/rowcount}")
-#    print(f"Last Change: {change}")
-    print(f"Last Change: {change_tot}")
-    print(f"Average Change: {change_tot/(rowcount-1)}")
+    print(f"Average Change: {change_tot/(rowcount-1):.2f}")
     print(f"Greatest Increase: {greatest_increase_amt} on {greatest_increase_mon}")
     print(f"Greatest Decrease: {greatest_decrease_amt} on {greatest_decrease_mon}")
 
@@ -72,12 +76,12 @@ with open(mainpath) as csvfile:
 
     # Initialize csv.writer
         csvwriter = csv.writer(csvfile)
-        #, delimiter='')
 
+    #   Print to output file  
         csvwriter.writerow([f"Financial Analysis"])
         csvwriter.writerow([f"------------------"])
         csvwriter.writerow([f"Total months: {rowcount}"])
         csvwriter.writerow([f"Total: {run_tot}"])
-        csvwriter.writerow([f"Average Change: {change_tot/(rowcount-1)}"])
+        csvwriter.writerow([f"Average Change: {change_tot/(rowcount-1):.2f}"])
         csvwriter.writerow([f"Greatest Increase: {greatest_increase_amt} on {greatest_increase_mon}"])
         csvwriter.writerow([f"Greatest Decrease: {greatest_decrease_amt} on {greatest_decrease_mon}"])
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,2 +1,78 @@
-# python-challenge
-python-challenge
+# First we'll import the os module
+# This will allow us to create file paths across operating systems
+import os
+
+# Module for reading CSV files
+import csv
+
+# csvpath = os.path.join('Resources', 'Netflix.csv')
+mainpath = os.path.join('Resources', 'election_data.csv')
+output_path = os.path.join('Resources', 'Polling_Results.txt')
+
+# with open (csvpath) as csvfile:
+with open(mainpath, newline='') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first 
+    csv_header = next(csvreader)
+    print(f"Reading in: {csv_header}. . . please wait")
+
+# Init some variables to zero
+    rowcount = 0    # so I know how many votes there are
+    candidate = []  # a list to keep the names of the candidates
+    votes = []     # a list to keep all of the votes for the candidates
+
+# Read each row of data after the header
+    for row in csvreader:
+        rowcount = rowcount + 1
+        
+        # check if this is a new candidate by looking in the list for the name
+        if (row[2]) in candidate:
+
+        # If in the votes list, increment by 1 the votes for this specific candidate
+            votes[candidate.index(row[2])] += 1
+        else:
+
+        # else, add this candidate to the voter list    
+            candidate.extend([row[2]])
+
+        # add a new variable to the voter array with an initial value of 1    
+            votes.extend([1])          
+#            votes[candidate.index(row[2])] += 1 # adding
+
+    print()
+    print(f"Election Results")
+    print(f"------------------------------------------")
+    print(f"Total Votes: {rowcount}")
+    winner_tot = 0 
+    winner = "nobody"
+    for name in candidate: 
+        print(f"{name} has {votes[candidate.index(name)]} votes, for {((votes[candidate.index(name)])/rowcount*100):.3f}%")
+        if votes[candidate.index(name)]>winner_tot:
+            winner = name
+            winner_tot = votes[candidate.index(name)]
+    print(f"------------------------------------------")
+    print(f"{winner} wins with {winner_tot} votes.")
+    print(f"------------------------------------------")
+
+    with open(output_path, 'w', newline='') as csvfile:
+
+    # Initialize csv.writer
+        csvwriter = csv.writer(csvfile)
+
+        csvwriter.writerow([f"Election Results"])
+        csvwriter.writerow([f"------------------------------------------"])
+        csvwriter.writerow([f"Total Votes: {rowcount}"])
+        winner_tot = 0 
+        winner = "nobody"
+        for name in candidate: 
+            csvwriter.writerow([f"{name} has {votes[candidate.index(name)]} votes, for {((votes[candidate.index(name)])/rowcount*100):.3f}%"])
+            if votes[candidate.index(name)]>winner_tot:
+                winner = name
+                winner_tot = votes[candidate.index(name)]
+        csvwriter.writerow([f"------------------------------------------"])
+        csvwriter.writerow([f"{winner} wins with {winner_tot} votes."])
+        csvwriter.writerow([f"------------------------------------------"])
+
